6e43f6c8dcb02c44d7947cbaadb505703cc8e13a
thegamelessplayer7 <thegamelessplayer7@gmail.com>
2021-11-03 02:50:51 -0500

    display value variable

--- script.js
+++ script.js
@@ -16,62 +16,76 @@ const divide = function(a) {
 
 const display = document.getElementById('display');
 
+let displayValue = '';
+
 const one = document.getElementById('numOne');
 one.addEventListener('click', () => {
     display.innerHTML += '1';
+    displayValue = Number(display.innerHTML);
 })
 
 const two = document.getElementById('numTwo');
 two.addEventListener('click', () => {
     display.innerHTML += '2';
+    displayValue = Number(display.innerHTML);
 })
 
 const three = document.getElementById('numThree');
 three.addEventListener('click', () => {
     display.innerHTML += '3';
+    displayValue = Number(display.innerHTML);
 })
 
 const four = document.getElementById('numFour');
 four.addEventListener('click', () => {
     display.innerHTML += '4';
+    displayValue = Number(display.innerHTML);
 })
 
 const five = document.getElementById('numFive');
 five.addEventListener('click', () => {
     display.innerHTML += '5';
+    displayValue = Number(display.innerHTML);
 })
 
 const six = document.getElementById('numSix');
 six.addEventListener('click', () => {
     display.innerHTML += '6';
+    displayValue = Number(display.innerHTML);
 })
 
 const seven = document.getElementById('numSeven');
 seven.addEventListener('click', () => {
     display.innerHTML += '7';
+    displayValue = Number(display.innerHTML);
 })
 
 const eight = document.getElementById('numEight');
 eight.addEventListener('click', () => {
     display.innerHTML += '8';
+    displayValue = Number(display.innerHTML);
 })
 
 const nine = document.getElementById('numNine');
 nine.addEventListener('click', () => {
     display.innerHTML += '9';
+    displayValue = Number(display.innerHTML);
 })
 
 const zero = document.getElementById('numZero');
 zero.addEventListener('click', () => {
     display.innerHTML += '0';
+    displayValue = Number(display.innerHTML);
 })
 
 const clearBtn = document.getElementById('clear-btn');
 clearBtn.addEventListener('click', () => {
     display.innerHTML = '';
+    displayValue = Number(display.innerHTML);
 })
 
-let x = display.innerHTML;
+
+
 
 const operate = function(operator, num1, num2) {
     if (operator === "-") {
